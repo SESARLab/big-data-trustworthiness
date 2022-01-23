686cde78e53702e617e14df5adc658206f0f5a94
Filippo Berto <berto.f@protonmail.com>
2022-01-23 17:23:05 +0100

    Various tests

--- flake.nix
+++ flake.nix
@@ -2,22 +2,36 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (python3.withPackages (p: with p; [
-              apache-airflow
-              ipython
-              jupyterlab
-            ]))
-          ];
-        };
-      });
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      pythonVersion = pkgs.python39;
+    in
+    rec {
+      devShell = pkgs.mkShell rec {
+        venvDir = "venv";
+        dontUseVenvShellHook=1;
+        buildInputs =
+          (with pythonVersion.pkgs; [ virtualenv pip setuptools venvShellHook ]) ++
+          (with pkgs; [ spark hadoop ]);
+        postVenvCreation = ''
+          python -m pip install -r requirements.txt
+        '';
+        # postShellHook = ''
+        #   export AIRFLOW_HOME=`pwd`;
+        #   export JAVA_HOME="${pkgs.jdk8}";
+        # '';
+        postShellHook = ''
+          echo postShellHook
+          # allow pip to install wheels
+          unset SOURCE_DATE_EPOCH
+          export AIRFLOW_HOME=`pwd`;
+          export JAVA_HOME="${pkgs.jdk8}";
+        '';
+      };
+    });
 }
